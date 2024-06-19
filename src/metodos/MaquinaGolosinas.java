19179103990f1ad650ec119d8fe6d7f678a8e639
equipo <equipo@DESKTOP-938K5GE>
2024-06-19 10:41:48 -0500

    JFrame incomplet Clarooo que yo me jodaaaa NO no no no yo no

--- src/metodos/MaquinaGolosinas.java
+++ src/metodos/MaquinaGolosinas.java
@@ -15,7 +15,7 @@ public class MaquinaGolosinas {
     double ventas[][];
 
     public MaquinaGolosinas() {
-        this.nombresGolosinas = new String[][] {
+        this.nombresGolosinas = new String[][]{
             {"KitKat", "Chicles de fresa", "Lacasitos", "Palotes"},
             {"Kinder Bueno", "Bolsa variada Haribo", "Chetoos", "Twix"},
             {"Kinder Bueno", "M&M'S", "Papa Delta", "Chicles de menta"},
@@ -23,26 +23,26 @@ public class MaquinaGolosinas {
 
         };
 
-        this.precio = new double[][] {
+        this.precio = new double[][]{
             {1000, 200, 500, 950},
             {1800, 100, 120, 100},
             {1750, 130, 120, 800},
             {1500, 110, 720, 350}
 
         };
-        this.cantidad = new double[][] {
+        this.cantidad = new double[][]{
             {5, 5, 5, 5},
             {5, 5, 5, 5},
             {5, 5, 5, 5},
             {5, 5, 5, 5}
 
         };
-        this.ventas = new double[][] {
+        this.ventas = new double[][]{
             {0, 0, 0, 0},
             {0, 0, 0, 0},
             {0, 0, 0, 0},
             {0, 0, 0, 0}
-    };    
+        };
     }
 
     public String[][] getNombresGolosinas() {
@@ -68,10 +68,9 @@ public class MaquinaGolosinas {
     public void setCantidad(double[][] cantidad) {
         this.cantidad = cantidad;
     }
-    
+
     public void pedirGolosina(JTextField jtfFila, JTextField jtfColumna) {
- 
-        
+
         int fila = Integer.parseInt(jtfFila.getText());
         int columna = Integer.parseInt(jtfColumna.getText());
         String info;
@@ -80,51 +79,76 @@ public class MaquinaGolosinas {
             if (cantidad[fila][columna] > 0) {
                 cantidad[fila][columna]--;
                 ventas[fila][columna]++;
-                info="Has comprado: " + nombresGolosinas[fila][columna] + " por "+precio[fila][columna]+" pesitos.";
-  
-                JOptionPane.showMessageDialog(null,info);
+                info = "Has comprado: " + nombresGolosinas[fila][columna] + " por " + precio[fila][columna] + " pesitos.";
+
+                JOptionPane.showMessageDialog(null, info);
             } else {
-                info= "Lo siento, no quedan más " + nombresGolosinas[fila][columna];
-                JOptionPane.showMessageDialog(null,info );
+                info = "Lo siento, no quedan más " + nombresGolosinas[fila][columna];
+                JOptionPane.showMessageDialog(null, info);
             }
         } else {
-            JOptionPane.showMessageDialog(null,"Codigo invalido");
+            JOptionPane.showMessageDialog(null, "Codigo invalido");
         }
-        
-        
+
     }
-    
-public void mostrarGolosinas() {
+
+    public void mostrarGolosinas() {
         String mensaje = "Golosinas disponibles: \n";
         for (int i = 0; i < nombresGolosinas.length; i++) {
             for (int j = 0; j < nombresGolosinas[i].length; j++) {
-                mensaje += "Código: " + i + j + " - Nombre: " + nombresGolosinas[i][j] + " - Precio: " + precio[i][j] + " - Cantidad: " + cantidad[i][j]+"\n";
+                mensaje += "Código: " + i + j + " - Nombre: " + nombresGolosinas[i][j] + " - Precio: " + precio[i][j] + " - Cantidad: " + cantidad[i][j] + "\n";
             }
-        } JOptionPane.showMessageDialog(null,mensaje); 
+        }
+        JOptionPane.showMessageDialog(null, mensaje);
     }
 
- public void rellenarGolosinas() {
+    public void rellenarGolosinas() {
         String contrasena = JOptionPane.showInputDialog(
-        "Entre la contraseña");
+                "Entre la contraseña");
 
         if (contrasena.equals("Maquina2020")) {
-   
+
             String codigo = JOptionPane.showInputDialog(
-        "Ingrese el código de la golosina (fila y columna, ej: 20): ");
+                    "Ingrese el código de la golosina (fila y columna, ej: 20): ");
             int fila = Character.getNumericValue(codigo.charAt(0));
             int columna = Character.getNumericValue(codigo.charAt(1));
 
             if (fila >= 0 && fila < 4 && columna >= 0 && columna < 4) {
                 int nuevaCantidad = Integer.parseInt(JOptionPane.showInputDialog(
-                "Ingrese la nueva cantidad de " + nombresGolosinas[fila][columna] + ": "));
+                        "Ingrese la nueva cantidad de " + nombresGolosinas[fila][columna] + ": "));
                 cantidad[fila][columna] = nuevaCantidad;
-                String info="La cantidad de " + nombresGolosinas[fila][columna] + " ha sido actualizada a " + nuevaCantidad;
-                JOptionPane.showMessageDialog(null,info);
+                String info = "La cantidad de " + nombresGolosinas[fila][columna] + " ha sido actualizada a " + nuevaCantidad;
+                JOptionPane.showMessageDialog(null, info);
             } else {
-                JOptionPane.showMessageDialog(null,"Código inválido.");
+                JOptionPane.showMessageDialog(null, "Código inválido.");
             }
         } else {
-            JOptionPane.showMessageDialog(null,"Contraseña incorrecta.");
+            JOptionPane.showMessageDialog(null, "Contraseña incorrecta.");
         }
     }
+
+    public void ventasporGolo() {
+        String ventasT = "Las ventas por golosinas son: \n";
+        for (int i = 0; i < nombresGolosinas.length; i++) {
+            for (int j = 0; j < nombresGolosinas[i].length; j++) {
+                ventasT += " - Nombre: " + nombresGolosinas[i][j] + " - Ventas por golosina" + ventas[i][j] * precio[i][j] + "\n";
+
+            }
+        }JOptionPane.showMessageDialog(null,ventasT );
+    }
+    public void ventasEstadisticas() {
+        String estadistica;
+        double ventasTotales = 0;
+        for (int i = 0; i < nombresGolosinas.length; i++) {
+            for (int j = 0; j < nombresGolosinas[i].length; j++) {
+                ventasTotales += ventas[i][j] * precio[i][j];
+
+            }
+          
+        }  estadistica= "Las ventas y total acumulado son: \n"+ventasTotales;
+        JOptionPane.showMessageDialog(null,estadistica );
+         
+    }
+
+    
 }
