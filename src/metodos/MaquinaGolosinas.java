c54bce16abd0c520e74e16ffaa4efa6c28a84709
Jesus Velez <jesusymeri@gmail.com>
2024-06-18 22:49:03 -0500

    gregadas matrices, constructor, setter getter

--- src/metodos/MaquinaGolosinas.java
+++ src/metodos/MaquinaGolosinas.java
@@ -1,4 +1,3 @@
-
 package metodos;
 
 /**
@@ -6,5 +5,62 @@ package metodos;
  * @author jesus
  */
 public class MaquinaGolosinas {
+
+    String nombresGolosinas[][];
+    double precio[][];
+    double cantidad[][];
+
+    public MaquinaGolosinas() {
+        this.nombresGolosinas = new String[][] {
+            {"KitKat", "Chicles de fresa", "Lacasitos", "Palotes"},
+            {"Kinder Bueno", "Bolsa variada Haribo", "Chetoos", "Twix"},
+            {"Kinder Bueno", "M&M'S", "Papa Delta", "Chicles de menta"},
+            {"Lacasitos", "Crunch", "Milkybar", "KitKat"}
+
+        };
+
+        this.precio = new double[][] {
+            {1000, 200, 500, 950},
+            {1800, 100, 120, 100},
+            {1750, 130, 120, 800},
+            {1500, 110, 720, 350}
+
+        };
+        this.cantidad = new double[][] {
+            {5, 5, 5, 5},
+            {5, 5, 5, 5},
+            {5, 5, 5, 5},
+            {5, 5, 5, 5}
+
+        };
+
+    }
+
+    public String[][] getNombresGolosinas() {
+        return nombresGolosinas;
+    }
+
+    public void setNombresGolosinas(String[][] nombresGolosinas) {
+        this.nombresGolosinas = nombresGolosinas;
+    }
+
+    public double[][] getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double[][] precio) {
+        this.precio = precio;
+    }
+
+    public double[][] getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double[][] cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+   
+    
 
 }
